6bad943d4bc763fda85fe1bee64b72a543d41d94
Diego <136717693+ciphermalware@users.noreply.github.com>
2025-07-25 11:39:29 +0200

    Update xor.py

--- XOR/xor.py
+++ XOR/xor.py
@@ -17,7 +17,7 @@ def is_printable(data):
 
 def score_english(plaintext):
     """Score how likely the text is to be english"""
-    # Character frequency in english text 
+    
     char_freq = {
         'e': 12.7, 't': 9.1, 'a': 8.2, 'o': 7.5, 'i': 7.0, 'n': 6.7, 's': 6.3, 'h': 6.1, 'r': 6.0, 'd': 4.3,
         'l': 4.0, 'u': 2.8, 'c': 2.8, 'm': 2.4, 'w': 2.4, 'f': 2.2, 'g': 2.0, 'y': 2.0, 'p': 1.9, 'b': 1.5,
